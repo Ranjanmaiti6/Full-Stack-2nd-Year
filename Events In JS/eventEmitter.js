09fdec207ae8c7b3f3487f8fe12a77ea4615f43a
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-09-02 09:38:37 +0530

    Dom Simulation

--- Events In JS/eventEmitter.js
+++ Events In JS/eventEmitter.js
@@ -1,6 +1,7 @@
 const EventEmitter = require('events'); //class
 const myEmitter = new EventEmitter();   //object
-//use two method on and emit, use emit to trigger or invoc the event
+//use two method on and emit, use emit to 
+//trigger or invoc the event
 
 myEmitter.on('greet' , (name)=>{
     console.log(`Hello ${name}! Welcome to Node.js`)

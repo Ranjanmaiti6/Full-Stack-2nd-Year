819bd2ba6ac88a0ec88e9829ea34fb26a7a0a503
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-09-08 11:57:02 +0530

    Check Avalable or not

--- FS Module/FolderCreate/sample.js
+++ FS Module/FolderCreate/sample.js
@@ -5,3 +5,12 @@ console.log("Folder created succfully")
 //delete
 fs.rmSync('sample Folder');
 console.log("Folder Delete Succfully")
+
+
+//crearte 
+fs.writeFileSync('secAstudent.txt' , 'This is the expriment no 2 and Wite file' , 'utf8');
+if(fs.existsSync('secAstudent.txt')){
+    console.log("file exists")
+}else{
+    console.log("File not found, need to create this file")
+}

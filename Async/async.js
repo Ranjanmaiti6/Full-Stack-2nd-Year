6eb9cd32ffe639520f37fd60ec2a480cdc104fce
Ranjan Maiti <ranjanmaiti.tech@gmail.com>
2026-09-09 09:23:59 +0530

    Async File

---
+++ Async/async.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+fs.writeFile(
+    'sample.text' , 'welcome to full stack d3evelopment' , (err) => {
+        if(err){
+            console.log("\nError creating file: " , err);
+            return ;
+        }
+        console.log('\nFile created succfully!')
+    }
+)
+
+fs.readFile('sample.txt' , 'utf8' , (err,data) =>{
+    if(err){
+        console.log("\nError reading file: " , err);
+        return ;
+    }
+    if(data){
+        console.log('\nFile read succefully')
+        console.log(data);
+    }
+})
+
+//append
+fs.appendFile('sample.txt' , '\nSemester: 3' , (err) => {
+    if(err){
+        console.log("\nError Updating file" , err);
+    }else{
+        console.log("\n File updated successfully");
+    }
+
+})
+
+
+//Update read
+fs.readFile('sample.txt' , 'utf8' , (err,data) =>{
+    if(err){
+        console.log("Error while updateing read");
+        return ;
+    }
+    else{
+        console.log("Update read successfully")
+    }
+})
